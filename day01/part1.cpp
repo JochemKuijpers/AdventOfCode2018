90f9252e03a89a5d4333895a9d7b779750811d7a
Jochem Kuijpers <github.com@jochemkuijpers.nl>
2018-12-12 22:30:56 +0100

    Day 1 part 1

---
+++ day01/part1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+int main()
+{
+	std::ifstream input("part1.input");
+	std::string line;
+	int total = 0;
+	while (std::getline(input, line)) {
+		if (line.empty()) break;	
+		int diff = std::stoi(line, nullptr);
+		total += diff;
+	}
+	std::cout << total << "\n";
+}
+
